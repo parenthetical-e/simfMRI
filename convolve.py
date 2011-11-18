87a4c5c8c9700021265755e5450e0b5c9db5dd3e
Erik Peterson <Erik.Exists@gmail.com>
2011-11-18 16:39:26 -0700

    added a noisy option to convolve.w_double_gamma

--- convolve.py
+++ convolve.py
@@ -2,13 +2,17 @@
 import numpy as np
 import simfMRI
 
-def w_double_gamma(impulses):
+def w_double_gamma(impulses,noisy=False):
 	"""
 	Convolves impulses, a 1 or 2d (column-oriented) array, 
 	with the double gamma hemodynamic response function (hrf).  
 	"""
+	if noisy:
+		hrf = simfMRI.hrf.double_gamma(20)
+		hrf_params = None
+	else:
+		hrf,hrf_params = simfMRI.noisy_double_gamma(20)
 
-	hrf = simfMRI.hrf.double_gamma(20)
 	n_col = 0
 	try: 
 		n_col =	impulses.shape[1]
@@ -20,12 +24,12 @@ def w_double_gamma(impulses):
 		print('Impulses is not an array. Assume it is 1d.')
 	
 	if n_col == 1:
-		return np.convolve(impulses,hrf)
+		return np.convolve(impulses,hrf), hrf_params
 	else:
 		bold = np.zeros_like(impulses)
 		for ii in range(n_col):
 
 			bold[:,ii] = np.convolve(
 					impulses[:,ii],hrf)[0:impulses.shape[0]]
-		return bold
+		return bold, hrf_params
 
