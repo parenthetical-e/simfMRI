40107185b990074621a553a500c8bfa4b96f9a30
Erik Peterson <Erik.Exists@gmail.com>
2011-11-18 17:06:53 -0700

    Redid the way noisy HRF generation worked to maintain backwards compatibility with original double_gamma function and all experiments up till now.

--- hrf.py
+++ hrf.py
@@ -22,47 +22,38 @@ def double_gamma(width):
 	return np.array(hrf)
 
 
-def noisy_double_gamma(width):
+def param_double_gamma(width,a1,a2,b2,c):
 	"""
-	Returns a HRF (and its free parameters) after it randomly selects one 
-	of the free params from the cannonical double_gamma() adding white 
-	noise to it, centered around either:
-		a1: 6.0
-		a2: 12.0
-		b2: .9
-		c: .35
+	Returns a HRF based on params. For canonical parameters use,
+	(a1=6, a2=12.0, b2=0.9, c=0.35).
 	"""
-	import scipy.stats as stats
-	from math import fabs 
-		# absolute value
+
+	## The independent params are provided,
+	## calculate the dependent ones
+	b1 = b2
+	d1 = a1*b1
+	d2 = a2*b2
 
 	hrf = []
 	x = np.arange(width)
+	f = lambda x: (x/d1)**a1 * np.exp( (d1-x)/ b1) - c * (x/d2)**a2 *\
+			np.exp( (d2-x)/ b2)
+	[hrf.append(f(ii)) for ii in x]
+
+	return np.array(hrf)
+
+
+def preturb_params(params=dict(a1=6, a2=12.0, b2=0.9, c=0.35)):
+	"""
+	Add rescaled white noise to a randomly selected param.
+	"""
+	import scipy.stats as stats
 	
-	## Randomly pick a key/param and 
-	## add noise, then calc the HRF.
-	params = dict(a1=6, a2=12.0, b2=0.9, c=0.35)
 	keys = params.keys()
 	np.random.shuffle(keys) # inplace
 	par = params[keys[0]]
 	params[keys[0]] = stats.norm.rvs(loc=par,scale=par/4.)
-		# Add white noise scaled to 1/2 of loc
-		# fabs() taken to prevent any of the params 
-		# dropping below zero, which would be bad.
-		# Give the scale limitation this should only
-		# rarely, if ever, bias the sampling; I hope anyway.
-	
-	## HRF:
-	a1 = params['a1']
-	a2 = params['a2']
-	b2 = params['b2']
-	b1 = b2
-	c = params['c']
-	d1 = a1*b1
-	d2 = a2*b2
-	f = lambda x: (x/d1)**a1 * np.exp( (d1-x)/ b1) - c * (x/d2)**a2 *\
-			np.exp( (d2-x)/ b2)
-	[hrf.append(f(ii)) for ii in x] 
+		## Grab a random value from the normal curve
+		## with its SD reduced by 0.25
 
-	return np.array(hrf), params
-
+	return params
