a97a3feba5cfbef501f9972f432a84a84b88bbc2
Erik Peterson <Erik.Exists@gmail.com>
2013-02-11 23:58:56 -0600

    ….

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 __all__ = ['template','hrf','norm','misc','noise','mapreduce','io',
         'analysis','examples', 'timing']
 
-import template
+
+import exp_examples
+import expclass
+import runclass
 import hrf
 import misc
 import norm
 import noise
 import mapreduce
 import io
-import examples
 import timing
 import analysis
